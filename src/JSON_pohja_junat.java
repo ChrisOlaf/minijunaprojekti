9643e48109e2b66e0179b4305b6471174775c42e
Risto Rautanen <rautanen.r@gmail.com>
2017-10-17 14:14:51 +0300

    Varikon alustusta.

--- src/JSON_pohja_junat.java
+++ src/JSON_pohja_junat.java
@@ -21,30 +21,6 @@ public class JSON_pohja_junat {
     }
 
 
-    private static void lueJunanJSONData() {
-        String baseurl = "https://rata.digitraffic.fi/api/v1";
-
-        try {
-
-            URL url = new URL(baseurl+"/live-trains/station/HKI/LH");
-            ObjectMapper mapper = new ObjectMapper();
-
-            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
-
-            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
-
-            System.out.println(junat.get(0).getTrainNumber());
-
-            // Seuraavaa varten on toteutettava TimeTableRow luokka:
-            //System.out.println(junat.get(0).getTimeTableRows().get(0).getScheduledTime());
-
-            System.out.println("\n\n");
-            System.out.println(junat.get(0));
-
-        } catch (Exception ex) {
-            System.out.println(ex);
-        }
-    }
 
 }
 
--- src/Varikko.java
+++ src/Varikko.java
@@ -1,5 +1,36 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Varikko {
+    static String baseurl = "https://rata.digitraffic.fi/api/v1";
+//    static URL url = new URL(baseurl+"/live-trains/station/HKI/LH");
 
+    private static void lueJunanJSONData(String urlparam) {
+        URL url = new URL(baseurl+urlparam);
 
+        try {
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            CollectionType tarkempiListanTyyppi = mapper.getTypeFactory().constructCollectionType(ArrayList.class, Juna.class);
+
+            List<Juna> junat = mapper.readValue(url, tarkempiListanTyyppi);  // pelkkä List.class ei riitä tyypiksi
+
+            System.out.println(junat.get(0).getTrainNumber());
+
+            // Seuraavaa varten on toteutettava TimeTableRow luokka:
+            //System.out.println(junat.get(0).getTimeTableRows().get(0).getScheduledTime());
+
+            System.out.println("\n\n");
+            System.out.println(junat.get(0));
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
 
 }
