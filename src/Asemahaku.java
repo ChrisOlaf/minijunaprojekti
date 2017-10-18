536a46897c644da8a92af19dcfe203954938277c
arkohonen <ar.kohonen@gmail.com>
2017-10-18 15:56:57 +0300

    Asemahaku-luokan luonnos

--- src/Asemahaku.java
+++ src/Asemahaku.java
@@ -1,15 +1,82 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class Asemahaku {
 
-    public void hae() {
+    static String annettuAsema;
 
-        // TODO
+   public static void main(String[] args) {
+        hae();
+    }
+
+    public static void hae() {
+
+        // /live-trains/station/HKI/TPE
+        // /live-trains/station/<departure_station_code>/<arrival_station_code>?departure_date=<departure_date>&from=<from>&to=<to>&limit=<limit>
+        // Varikko.LueJunanJsonData(String URL)
+
+        Scanner lueAsema = new Scanner(System.in);
+
+        kysyAsema(lueAsema);
+
+        haeJunatAsemanMukaan();
+
+
+    }
+    public static void kysyAsema(Scanner lueAsema) {
+        System.out.println("Mikä asema? (kolmikirjaiminen asemakoodi, esim. HKI)");
+        annettuAsema = lueAsema.nextLine().toUpperCase();
+        while (annettuAsema.length() != 3) {
+            System.out.println("\nSyötä kolmikirjaiminen koodi, esim. HKI (Helsinki) tai TPE (Tampere).\n");
+            annettuAsema = lueAsema.nextLine().toUpperCase();
+        }
+    }
+
+    public static void haeJunatAsemanMukaan() {
+        String url = "/live-trains/station/" + annettuAsema + "?minutes_before_departure=120&minutes_before_arrival=120";
+
+        // hae uusin data Varikolle
+        Varikko.lueJunanJSONData(url);
+
+        // hae junien lista käyttöön paikalliseen muuttujaan
+        List<Juna> asemaJunat = Varikko.junat;
+
+        for (Juna j : asemaJunat) {
+            if (j.timeTableRows.get(0).getType().equals("DEPARTURE") && j.timeTableRows.get(0).getStationShortCode().equals(annettuAsema)) {
+                System.out.println(
+                        "Lähtevät: " + j.getTrainType() + j.getTrainNumber()
+                        + " Pääteasema: " + j.timeTableRows.get(j.timeTableRows.size()-1).getStationShortCode()
+                        + " Lähtöaika: " + j.timeTableRows.get(0).getScheduledTime()
+                        + " Junan tunnus, jos lähijuna: " + j.getCommuterLineID());
+
+            }
+        }
+
+        for (Juna j: asemaJunat) {
+
+            if(j.timeTableRows.get(j.timeTableRows.size()-1).getType().equals("ARRIVAL")) {
+                System.out.println(
+                        "Saapuvat: " + j.getTrainType() + j.getTrainNumber()
+                                + " Lähtöasema: " + j.timeTableRows.get(0).getStationShortCode()
+                                + " Saapumisaika: " + j.timeTableRows.get(j.timeTableRows.size()-1).getScheduledTime()
+                                + " Junan tunnus, jos lähijuna: " + j.getCommuterLineID());
+            }
+        }
+    }
+
+
+
+
+////                => näytä junien tiedot lajiteltuna saapuvat / lähtevät
+
+    // TODO
 
 //        URL: /live-trains/station/<station_shortcode>?arrived_trains=arrived_trains>&arriving_trains=arriving_trains> &departed_trains=<departed_trains>&departing_trains=<departing_trains>&version=<change_number>
 //        Esimerkki: /live-trains/station/HKI
 //        Esimerkki: /live-trains/station/HKI?arrived_trains=5&arriving_trains=5&departed_trains=5&departing_trains=5&include_nonstopping=false
 //
 
-        // Varikko.LueJunanJsonData(String URL)
-    }
+    // Varikko.LueJunanJsonData(String URL)
+
 
 }
