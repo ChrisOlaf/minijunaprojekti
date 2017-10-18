dad38de80fbe92f8ecac57bb1710a488b0209c6b
Risto Rautanen <rautanen.r@gmail.com>
2017-10-18 17:01:37 +0300

    Junahakuun toimiva kysely ja virheiden blokkaus.

--- src/Junahaku.java
+++ src/Junahaku.java
@@ -16,20 +16,24 @@ public class Junahaku {
         String jnaTyyppi = "IC";
         int jnaNumero = 404;
         Scanner jnaScnr = new Scanner(System.in);
+        System.out.println("Löydä haluamasi junan aikataulu. Anna junan tunnus, esim: IC 404.");
 
         junatunnus:
         for (; ; ) {
 
-            System.out.println("Löydä haluamasi junan aikataulu. Anna junan tunnus, esim: IC 404.");
             String jnaHaku = jnaScnr.nextLine().toUpperCase();
             jnaTyyppi = jnaHaku.replaceAll("[^A-Z]", "");
-            jnaNumero = Integer.parseInt(jnaHaku.replaceAll("[^0-9]", ""));
+            try {
+                jnaNumero = Integer.parseInt(jnaHaku.replaceAll("[^0-9]", ""));
+            } catch (NumberFormatException e) {
+                System.out.println("Junan numero puuttui. Yritä uudelleen!\n");
+                continue;
+            }
             System.out.println("Tehdään haku junatunnuksella: " + jnaTyyppi + jnaNumero + ".\n");
-//            System.out.println(jnaTyyppi+jnaNumero);
-            String url = "/trains/latest/" + jnaNumero;
-            System.out.println(url);
 
+            String url = "/trains/latest/" + jnaNumero;         // Kootaan URL josta dataa haetaan.
             Varikko.lueJunanJSONData(url);
+
             if (Varikko.junat.size() == 0) {                    // Tarkistetaan onko junan numerolla taulukkoa.
                 System.out.println("Antamallasi tunnuksella ei löytynyt junaa. Anna tunnus uudelleen.\n");
                 continue;
@@ -37,7 +41,7 @@ public class Junahaku {
 
             for (Juna j : Varikko.junat) {                      // Käydään läpi löytyykö junan tyyppi+numero -yhdistelmällä tietoa.
                 if (jnaTyyppi.equals(j.getTrainType()) && (jnaNumero == j.getTrainNumber())) {
-                    System.out.println(j.getTrainType() + j.getTrainNumber() + " lähtee " + j.getDepartureDate());
+                    System.out.println(j.getTrainType() + j.getTrainNumber() + " lähtee " + j.getTimeTableRows().get(0).getScheduledTime());
                     break junatunnus;
                 }
                 System.out.println("Antamallasi tunnuksella ei löytynyt junaa. Anna tunnus uudelleen.\n");
