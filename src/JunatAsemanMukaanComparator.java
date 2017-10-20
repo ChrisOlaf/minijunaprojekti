c1607b7d24be444d1c66f6822cb8d8cdddef5841
Titta K <titta@maili.com>
2017-10-20 08:09:34 +0300

    Comparator kommentit lisäys

--- src/JunatAsemanMukaanComparator.java
+++ src/JunatAsemanMukaanComparator.java
@@ -2,18 +2,20 @@ import java.util.Comparator;
 import java.util.Date;
 /**
  *
- *Luokkaa käytetään, jotta asemahaussa saadut junat saadaan järjestettyä ajan mukaan.
- *
+ *JunatAsemanMukaanComparator-luokkaa käytetään, jotta asemahaussa
+ * saadut junat saadaan järjestettyä ajan mukaan.
  *
  * @author Titta Kivikoski
  */
 class JunatAsemanMukaanComparator implements Comparator<Juna> {
     @Override
     public int compare(Juna a, Juna b) {
+//Asemahaussa käyttäjältä kysytään asemaa (annettuAsema).
         String asema = Asemahaku.annettuAsema;
         long aikaerotus;
         Date aAika = null, bAika = null;
-
+//Käydään läpi juna-olioiden TimeTableRows-lista, josta löytyy junan aseman StationShortCode.
+//Katsotaan, täsmääkö se annettuun asemaan. Jos täsmää, otetaan aika.
         for (TimeTableRow t : a.getTimeTableRows()) {
             if (t.getStationShortCode().equals(asema)) {
                 aAika = t.getTime();
@@ -24,7 +26,7 @@ class JunatAsemanMukaanComparator implements Comparator<Juna> {
                 bAika = t.getTime();
             }
         }
-
+// Vertaillaan junien aikoja aikaerotuksen avulla.
         aikaerotus = aAika.getTime() - bAika.getTime();
 
         if (aikaerotus == 0) {
